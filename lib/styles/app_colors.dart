3305884e4867639abffae296940cb6529872f2a7
Cao Nam <nam52871@donga.edu.vn>
2023-09-22 20:31:07 +0700

    Setup project

---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff00CEA6);
+  static const secondary = Color(0xFFFFC000);
+  static const black = Color(0xFF121212);
+  static const white = Color(0xFFFAF8FF);
+  static const colorTextDarker = Color(0xFF333333);
+  static const colorTextDark = Color(0xFF777777);
+}
---
+++ lib/styles/app_text.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppText {
+  static const display =
+      TextStyle(fontSize: 48, fontWeight: FontWeight.w500); //Medium
+  static const heading1 =
+      TextStyle(fontSize: 34, fontWeight: FontWeight.w600); //Semibold
+  static const heading2 =
+      TextStyle(fontSize: 24, fontWeight: FontWeight.w600); //Medium
+  static const heading3 =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600); //Semibold
+  static const body1 =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.w400); //Regular
+  static const body2 =
+      TextStyle(fontSize: 15, fontWeight: FontWeight.w400); //Regular
+  static const caption =
+      TextStyle(fontSize: 14, fontWeight: FontWeight.w400); //Regular
+  static const helperText =
+      TextStyle(fontSize: 12, fontWeight: FontWeight.w400); //Regular
+  static const quote =
+      TextStyle(fontSize: 28, fontWeight: FontWeight.w400); //Regular
+}
